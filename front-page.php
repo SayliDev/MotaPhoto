7033f9b369c2d747fab3f1af89ca39ab766540ee
Artax601 <artaxw1@gmail.com>
2024-02-06 23:59:43 +0100

    feat: Intégration du héro + Script
    - Intégration du héro avec le script de chargement d’une image aléatoire du catalogue en background.
    
    --> Version bump à 1.5.5

--- front-page.php
+++ front-page.php
@@ -2,8 +2,27 @@
 
 get_header();
 ?>
+<div class="hero-header">
+    <div class="hero-header__image"> <?php
 
-    
+    $args = array(
+        'posts_per_page' => -1,
+        'post_type' => 'photos', 
+        'post_status' => 'publish',
+        'order' => 'ASC', // par ordre croissant
+    );
+
+  $posts = get_posts($args);
+
+  // sélectionne post aléatoire
+  $random_post = $posts[array_rand($posts) ];
+
+  // récupére l'image aléatoire
+  echo get_the_post_thumbnail($random_post->ID, 'full' );
+?></div>    
+    <h1 class="hero-header__title">PHOTOGRAPHE EVENT</h1>
+
+</div>
 
 <?php
-get_footer();
+get_footer(); 
